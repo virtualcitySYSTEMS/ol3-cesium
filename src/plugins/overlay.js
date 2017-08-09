74348849b701bde0f66211c8616e891440d04bfb
Ben Kuster <bkuster@virtualcitysystems.de>
2017-08-09 11:30:01 +0200

    compiler safe prototype

--- src/plugins/overlay.js
+++ src/plugins/overlay.js
@@ -1,33 +1,21 @@
 goog.provide('olcs.Overlay');
 
 goog.require('ol');
+goog.require('ol.proj');
 goog.require('ol.dom');
-goog.require('ol.Overlay');
 goog.require('ol.Observable');
 
 /**
- * @classdesc
- * An element to be displayed over the map and attached to a single map
- * location.  Like {@link ol.control.Control}, Overlays are visible widgets.
- * Unlike Controls, they are not in a fixed position on the screen, but are tied
- * to a geographical coordinate, so panning the map will move an Overlay but not
- * a Control.
- *
- * Example:
- *
- *     var popup = new olcs.Overlay({
- *       element: document.getElementById('popup')
- *     });
- *     popup.setPosition(coordinate);
- *     map.addOverlay(popup);
- *
  * @constructor
- * @extends {ol.Overlay}
- * @param {olx.OverlayOptions} options Overlay options.
+ * @param {{
+ *  scene: !Cesium.Scene,
+ *  parent: !ol.Overlay,
+ *  synchronizer: !olcs.OverlaySynchronizer
+ * }} options Overlay options.
  * @api
+ * @todo find a way to retrieve the parent stopEvent flag and others
  */
 olcs.Overlay = function(options) {
-  ol.Overlay.call(this, options);
   /**
    * @private
    * @type {?Function}
@@ -35,52 +23,81 @@ olcs.Overlay = function(options) {
   this.scenePostRenderListenerRemover_ = null;
 
   /**
-   * @type {Cesium.Scene}
+   * @type {!Cesium.Scene}
    */
   this.scene = options.scene;
 
-  /** @type {olcs.OverlaySynchronizer}*/
+  /** @type {!olcs.OverlaySynchronizer} */
   this.synchronizer = options.synchronizer;
 
   /**
    * @private
    * @type {boolean}
+   * @todo read from parent
    */
-  this.insertFirst_ = options.insertFirst !== undefined ?
-    options.insertFirst : true;
+  this.insertFirst_ = true;
 
   /**
    * @private
    * @type {boolean}
+   * @todo read from parent
    */
-  this.stopEvent_ = options.stopEvent !== undefined ? options.stopEvent : true;
+  this.stopEvent_ = true;
 
+  /**
+   * @private
+   * @type {Element}
+   */
+  this.element_ = document.createElement('DIV');
+  this.element_.className = `ol-overlay-container ${ol.css.CLASS_SELECTABLE}`;
+  this.element_.style.position = 'absolute';
+
+  /**
+   * @private
+   * @type {!ol.Overlay}
+   */
+  this.parent_ = options['parent'];
+  /** @type {ol.Coordinate|undefined} */
+  this.position = this.parent_.getPosition();
+  /** @type {Element|undefined} */
+  this.element = this.parent_.getElement();
+  /**
+   * @private
+   * @type {{bottom_: string,
+   *         left_: string,
+   *         right_: string,
+   *         top_: string,
+   *         visible: boolean}}
+   */
+  this.rendered_ = {
+    bottom_: '',
+    left_: '',
+    right_: '',
+    top_: '',
+    visible: true
+  };
   /**
    * @type {MutationObserver|null}
    * @private
    */
   this.observer_ = null;
 
-  /**
-   * @private
-   * @type {Element}
-   * TODO I'm unsure if this works with the inherited functions
-   */
-  this.element_ = document.createElement('DIV');
-  this.element_.className = `ol-overlay-container ${ol.css.CLASS_SELECTABLE}`;
-  this.element_.style.position = 'absolute';
+  if (this.parent_.getElement()) {
+    this.addMutationObserver_(this.parent_.getElement().parentNode);
+  }
 
-  this.autoPanMargin_ = options['autoPanMargin_'];
-
-  /**
-   * @private
-   * @type {ol.Overlay}
-   */
-  this.parent_ = options['parent'];
-  /** @type {Array<number>} */
+  /** @type {Array<ol.EventsKey>} */
   this.listenerKeys_ = [];
-  if (this.parent_) {
-    const target = this.parent_.getElement().parentNode;
+  this.listenerKeys_.push(this.parent_.on('change:position', this.changePosition_.bind(this)));
+  this.listenerKeys_.push(this.parent_.on('change:element', this.changeElement_.bind(this)));
+};
+
+/**
+ * @param {Node} target
+ * @private
+ */
+olcs.Overlay.prototype.addMutationObserver_ = function(target) {
+  if (this.observer_ === null) {
     this.observer_ = new MutationObserver(this.changeElement_.bind(this));
     this.observer_.observe(target, {
       attributes: false,
@@ -88,16 +105,13 @@ olcs.Overlay = function(options) {
       characterData: false,
       subtree: true
     });
-    this.listenerKeys_.push(this.parent_.on('change:position', this.changePosition_.bind(this)));
-    this.listenerKeys_.push(this.parent_.on('change:element', this.changeElement_.bind(this)));
   }
 };
-ol.inherits(olcs.Overlay, ol.Overlay);
 
 /**
  * Get the scene associated with this overlay.
  * @see ol.Overlay.prototype.getMap
- * @return {Cesium.Scene|undefined} The map that the overlay is part of.
+ * @return {!Cesium.Scene} The scene that the overlay is part of.
  * @observable
  * @api
  */
@@ -105,17 +119,17 @@ olcs.Overlay.prototype.getScene = function() {
   return this.scene;
 };
 
-// /**
-//  * @protected
-//  */
-// olcs.Overlay.prototype.handleElementChanged = function() { // NOTE this is setElement in overlaysynchronizer
-//   ol.dom.removeChildren(this.element_);
-//   var element = this.getElement();
-//   if (element) {
-//     this.element_.appendChild(element);
-//   }
-// };
-
+/**
+ * Modify the visibility of the element.
+ * @param {boolean} visible Element visibility.
+ * @protected
+ */
+olcs.Overlay.prototype.setVisible = function(visible) {
+  if (this.rendered_.visible !== visible) {
+    this.element_.style.display = visible ? '' : 'none';
+    this.rendered_.visible = visible;
+  }
+};
 
 /**
  * @api
@@ -128,7 +142,7 @@ olcs.Overlay.prototype.handleMapChanged = function() {
 
   const scene = this.getScene();
   if (scene) {
-    this.scenePostRenderListenerRemover_ = scene.postRender.addEventListener(this.render.bind(this));
+    this.scenePostRenderListenerRemover_ = scene.postRender.addEventListener(this.updatePixelPosition.bind(this));
     this.updatePixelPosition();
     const container = this.stopEvent_ ?
       this.synchronizer.getOverlayContainerStopEvent() : this.synchronizer.getOverlayContainer(); // TODO respect stop-event flag in synchronizer
@@ -141,8 +155,8 @@ olcs.Overlay.prototype.handleMapChanged = function() {
 };
 
 /**
- * @protected
- * @todo potentially we don't need this at all
+ * @todo potentially, we don't need this at all?
+ * @todo see to the panIntoView option
  */
 olcs.Overlay.prototype.handlePositionChanged = function() {
   this.updatePixelPosition();
@@ -151,40 +165,96 @@ olcs.Overlay.prototype.handlePositionChanged = function() {
   // }
 };
 
-olcs.Overlay.prototype.panIntoView_ = function() {
-  const position = this.getPosition();
-  let cartesian;
-  if (position.length === 2) {
-    cartesian = Cesium.Cartesian3.fromDegreesArray(position)[0];
-  } else {
-    cartesian = Cesium.Cartesian3.fromDegreesArrayHeights(position)[0];
+/**
+ * @protected
+ */
+olcs.Overlay.prototype.handleElementChanged = function() {
+  ol.dom.removeChildren(this.element_);
+  const element = this.getElement();
+  if (element) {
+    this.element_.appendChild(element);
   }
-
-  const pixelCartesian = this.scene.cartesianToCanvasCoordinates(cartesian);
-  const element = this.element_;
-  const overlayRect = this.getRect_(element,
-    [ol.dom.outerWidth(element), ol.dom.outerHeight(element)]);
-
-
-  console.log(pixelCartesian, overlayRect);
 };
 
-olcs.Overlay.prototype.getRect_ = function(element, size) {
-  const box = element.getBoundingClientRect();
-  const offsetX = box.left + window.pageXOffset;
-  const offsetY = box.top + window.pageYOffset;
-  return [
-    offsetX,
-    offsetY,
-    offsetX + size[0],
-    offsetY + size[1]
-  ];
+/**
+ * @private
+ * @todo check if this is needed or even possible
+ */
+olcs.Overlay.prototype.panIntoView_ = function() {
+  // const position = this.getPosition();
+  // let cartesian;
+  // if (position.length === 2) {
+  //   cartesian = Cesium.Cartesian3.fromDegreesArray(position)[0];
+  // } else {
+  //   cartesian = Cesium.Cartesian3.fromDegreesArrayHeights(position)[0];
+  // }
+  //
+  // const pixelCartesian = this.scene.cartesianToCanvasCoordinates(cartesian);
+  // const element = this.element_;
+  // const overlayRect = this.getRect_(element,
+  //   [ol.dom.outerWidth(element), ol.dom.outerHeight(element)]);
+  //
+
 };
 
+/**
+ * Sets up a freshly created overlay
+ * @api
+ * @todo not needed? just call these functions on creation?
+ */
 olcs.Overlay.prototype.init = function() {
   this.changePosition_();
   this.changeElement_();
+  this.handleMapChanged();
 };
+
+/**
+ * @return {ol.Coordinate|undefined}
+ * @api
+ */
+olcs.Overlay.prototype.getPosition = function() {
+  return this.position;
+};
+
+/**
+ * @return {Array.<number>}
+ * @api
+ */
+olcs.Overlay.prototype.getOffset = function() {
+  return this.parent_.getOffset();
+};
+
+/**
+ * @return {ol.OverlayPositioning}
+ * @api
+ */
+olcs.Overlay.prototype.getPositioning = function() {
+  return this.parent_.getPositioning();
+};
+
+/**
+ * @return {Element|undefined}
+ */
+olcs.Overlay.prototype.getElement = function() {
+  return this.element;
+};
+
+/**
+ * @param {ol.Coordinate} position
+ * @todo what does this do? Shouldn't this trigger something?
+ */
+olcs.Overlay.prototype.setPosition = function(position) {
+  this.position = position;
+};
+
+/**
+ * @param {Element} element
+ */
+olcs.Overlay.prototype.setElement = function(element) {
+  this.element = element;
+  this.handleElementChanged();
+};
+
 /**
  * @private
  */
@@ -192,13 +262,14 @@ olcs.Overlay.prototype.changePosition_ = function() {
   const position = this.parent_.getPosition();
 
   if (position) {
-    this.element_.style.display = '';
+    this.setVisible(true);
     const sourceProjection = this.parent_.getMap().getView().getProjection();
     const coords = ol.proj.transform(position, sourceProjection, 'EPSG:4326');
     this.setPosition(coords);
     this.handleMapChanged();
   } else {
-    this.element_.style.display = 'none';
+    this.setPosition(position);
+    this.setVisible(false);
   }
 };
 
@@ -232,19 +303,22 @@ olcs.Overlay.prototype.changeElement_ = function() {
 
     this.setElement(clonedNode);
     const parentNode = this.getElement().parentNode;
-    while (parentNode.firstChild) {
-      parentNode.removeChild(parentNode.firstChild);
-    }
-    const childNodes = this.parent_.getElement().parentNode.childNodes;
-    for (let i = 0; i < childNodes.length; i++) {
-      cloneNode(childNodes[i], parentNode);
+    if (parentNode) {
+      this.addMutationObserver_(parentNode);
+      while (parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild);
+      }
+      const childNodes = this.parent_.getElement().parentNode.childNodes;
+      for (let i = 0; i < childNodes.length; i++) {
+        cloneNode(childNodes[i], parentNode);
+      }
     }
   }
 };
 
 /**
  * Update pixel position.
- * @protected
+ * @suppress {checkTypes, accessControls}
  */
 olcs.Overlay.prototype.updatePixelPosition = function() {
   const scene = this.getScene();
@@ -263,15 +337,20 @@ olcs.Overlay.prototype.updatePixelPosition = function() {
   const pixelCartesian = scene.cartesianToCanvasCoordinates(cartesian);
   const pixel = [pixelCartesian.x, pixelCartesian.y];
   const mapSize = [scene.canvas.width, scene.canvas.height];
-  this.updateRenderedPosition(pixel, mapSize);
+  const that = /** @type {ol.Overlay} */ (this);
+  ol.Overlay.prototype.updateRenderedPosition.call(that, pixel, mapSize);
 };
 
+/**
+ * Destroys the overlay, removing all its listeners and elements
+ * @api
+ */
 olcs.Overlay.prototype.destroy = function() {
   if (this.observer_) {
     this.observer_.disconnect();
     this.observer_ = null;
   }
-  this.listenerKeys_.forEach(ol.Observable.unByKey);
+  ol.Observable.unByKey(this.listenerKeys_);
   this.listenerKeys_.splice(0);
   if (this.element_.removeNode) {
     this.element_.removeNode(true);
